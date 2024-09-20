109bbfe5bb948ffa6c5cc9957c8509523922b866
KhoeTrinh <kelvintrinh09@gmail.com>
2024-09-20 20:27:22 +0700

    yea boy

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,37 @@
-type Props = {};
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+import DesignConfigurator from '@/app/configure/design/DesignConfigurator';
 
-const Page = async (props: Props) => {
-    return <div></div>;
+interface Props {
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    };
+}
+
+const Page = async ({ searchParams }: Props) => {
+    const { id } = searchParams;
+
+    if (!id || typeof id !== 'string') {
+        return notFound();
+    }
+
+    const configuration = await db.configuration.findUnique({
+        where: { id },
+    });
+
+    if (!configuration) {
+        return notFound();
+    }
+
+    const { imageUrl, width, height } = configuration;
+
+    return (
+        <DesignConfigurator
+            configId={configuration.id}
+            imageDimensions={{ width, height }}
+            imageUrl={imageUrl}
+        />
+    );
 };
 
 export default Page;
