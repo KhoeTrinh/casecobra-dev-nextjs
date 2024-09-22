89fd94eb716347177bf47d52f72d70f63082082c
KhoeTrinh <kelvintrinh09@gmail.com>
2024-09-22 21:57:20 +0700

    wow wow wow

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -2,11 +2,12 @@
 
 import { previews } from '@/components/array';
 import Phone from '@/components/Phone';
+import { Button } from '@/components/ui/button';
 import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products';
 import { cn, formatPrice } from '@/lib/utils';
 import { COLORS, MODELS } from '@/validators/option-validator';
 import { Configuration } from '@prisma/client';
-import { Check } from 'lucide-react';
+import { ArrowRight, Check } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import Confetti from 'react-dom-confetti';
 
@@ -25,6 +26,11 @@ const DesignPreview = ({ configuration }: Props) => {
     const { label: modelLabel } = MODELS.options.find(
         ({ value }) => value === model
     )!;
+    let totalPrice = BASE_PRICE;
+    if (material === 'polycarbonate')
+        totalPrice += PRODUCT_PRICES.material.polycarbonate;
+    if (finish === 'textured')
+        totalPrice += PRODUCT_PRICES.finish.textured;
 
     return (
         <>
@@ -65,73 +71,86 @@ const DesignPreview = ({ configuration }: Props) => {
                         <Check className='h-4 w-4 text-green-500' />
                         In stock and ready to ship
                     </div>
-
-                    <div className='sm:col-span-12 md:col-span-9 text-base'>
-                        <div
-                            className='grid grid-cols-1 gap-y-8 border-b 
+                </div>
+                <div className='sm:col-span-12 md:col-span-9 text-base'>
+                    <div
+                        className='grid grid-cols-1 gap-y-8 border-b 
                         border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 
                         sm:py-6 md:py-10'
-                        >
-                            {previews.map((preview, index) => (
-                                <div key={index}>
-                                    <p className='font-medium text-zinc-950'>
-                                        {preview.title}
-                                    </p>
-                                    <ol className='mt-3 text-zinc-700 list-disc list-inside'>
-                                        {preview.description.map(
-                                            (preview2, index) => (
-                                                <li key={index}>
-                                                    {preview2}
-                                                </li>
-                                            )
-                                        )}
-                                    </ol>
-                                </div>
-                            ))}
-                        </div>
+                    >
+                        {previews.map((preview, index) => (
+                            <div key={index}>
+                                <p className='font-medium text-zinc-950'>
+                                    {preview.title}
+                                </p>
+                                <ol className='mt-3 text-zinc-700 list-disc list-inside'>
+                                    {preview.description.map(
+                                        (preview2, index) => (
+                                            <li key={index}>{preview2}</li>
+                                        )
+                                    )}
+                                </ol>
+                            </div>
+                        ))}
+                    </div>
 
-                        <div className='mt-8'>
-                            <div className='bg-gray-50 p-6 sm:rounded-lg sm:p-8'>
-                                <div className='flow-root text-sm'>
+                    <div className='mt-8'>
+                        <div className='bg-gray-50 p-6 sm:rounded-lg sm:p-8'>
+                            <div className='flow-root text-sm'>
+                                <div className='flex items-center justify-between py-1 mt-2'>
+                                    <p className='text-gray-600'>
+                                        Base price
+                                    </p>
+                                    <p className='font-medium text-gray-900'>
+                                        {formatPrice(BASE_PRICE / 100)}
+                                    </p>
+                                </div>
+
+                                {finish === 'textured' ? (
                                     <div className='flex items-center justify-between py-1 mt-2'>
                                         <p className='text-gray-600'>
-                                            Base price
+                                            Textured Finish
                                         </p>
                                         <p className='font-medium text-gray-900'>
-                                            {formatPrice(BASE_PRICE / 100)}
+                                            {formatPrice(
+                                                PRODUCT_PRICES.finish
+                                                    .textured / 100
+                                            )}
                                         </p>
                                     </div>
+                                ) : null}
 
-                                    {finish === 'textured' ? (
-                                        <div className='flex items-center justify-between py-1 mt-2'>
-                                            <p className='text-gray-600'>
-                                                Textured Finish
-                                            </p>
-                                            <p className='font-medium text-gray-900'>
-                                                {formatPrice(
-                                                    PRODUCT_PRICES.finish
-                                                        .textured / 100
-                                                )}
-                                            </p>
-                                        </div>
-                                    ) : null}
+                                {material === 'polycarbonate' ? (
+                                    <div className='flex items-center justify-between py-1 mt-2'>
+                                        <p className='text-gray-600'>
+                                            Soft polycarbonate material
+                                        </p>
+                                        <p className='font-medium text-gray-900'>
+                                            {formatPrice(
+                                                PRODUCT_PRICES.material
+                                                    .polycarbonate / 100
+                                            )}
+                                        </p>
+                                    </div>
+                                ) : null}
 
-                                    {material === 'polycarbonate' ? (
-                                        <div className='flex items-center justify-between py-1 mt-2'>
-                                            <p className='text-gray-600'>
-                                                Soft polycarbonate material
-                                            </p>
-                                            <p className='font-medium text-gray-900'>
-                                                {formatPrice(
-                                                    PRODUCT_PRICES.material
-                                                        .polycarbonate /
-                                                        100
-                                                )}
-                                            </p>
-                                        </div>
-                                    ) : null}
+                                <div className='my-2 h-px bg-gray-200' />
+                                <div className='flex items-center justify-between py-2'>
+                                    <p className='font-semibold text-gray-900'>
+                                        Order total
+                                    </p>
+                                    <p className='font-semibold text-gray-900'>
+                                        {formatPrice(totalPrice / 100)}
+                                    </p>
                                 </div>
                             </div>
+                        </div>
+
+                        <div className='mt-8 flex justify-end pb-12'>
+                            <Button className='px-4 sm:px-6 lg:px-8'>
+                                Check out{' '}
+                                <ArrowRight className='h-4w-4 ml-1.5 inline' />
+                            </Button>
                         </div>
                     </div>
                 </div>
