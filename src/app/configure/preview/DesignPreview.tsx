de37b8882dd3655b2b425565493a190f67f15c49
KhoeTrinh <kelvintrinh09@gmail.com>
2024-09-23 11:34:37 +0700

    hoshi

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -7,6 +7,7 @@ import { BASE_PRICE, PRODUCT_PRICES } from '@/config/products';
 import { cn, formatPrice } from '@/lib/utils';
 import { COLORS, MODELS } from '@/validators/option-validator';
 import { Configuration } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
 import { ArrowRight, Check } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import Confetti from 'react-dom-confetti';
@@ -31,6 +32,11 @@ const DesignPreview = ({ configuration }: Props) => {
         totalPrice += PRODUCT_PRICES.material.polycarbonate;
     if (finish === 'textured')
         totalPrice += PRODUCT_PRICES.finish.textured;
+
+    const {} = useMutation({
+        mutationKey: ['get-checkout-session'],
+        mutationFn: 
+    })
 
     return (
         <>
